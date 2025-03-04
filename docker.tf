dde9eb62792e7c402330a947d355afdf11d63faa
Pallavi C <info@joindevops.com>
2025-03-04 22:06:03 +0530

    expense-docker

---
+++ docker.tf
@@ -0,0 +1,55 @@
+resource "aws_instance" "this" {
+  #count = 3
+  ami                    = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t3.micro"
+  # 20 GB is not enough
+  root_block_device {
+    volume_size = 50 # set root volume size to 50GB
+    volume_type = "gp3"  # use gp3 for better performance(optional)
+  }
+  user_data = file("docker.sh")
+  tags = {
+    Name    = "docker"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+output "docker_ip" {
+  value       = aws_instance.this.public_ip
+}
